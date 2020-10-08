ea9df118525fad8705710a5ffacacc27a18a2e88
Max Trauboth <max@trauboth.com>
2020-10-08 22:02:34 +0200

    Initial Commit

---
+++ AccessibleSlider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AccessibleSlider
+//
+//  Created by Max Trauboth on 08.10.20.
+//  Copyright © 2020 Max Trauboth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
